f2d32fb27fad38fa6acbbb03984f629b07d9eecc
mego354 <143918350+mego354@users.noreply.github.com>
2025-03-23 07:58:39 +0200

    Car app

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +27,7 @@ SECRET_KEY = 'django-insecure-hrgd!+f3+7z2bq#no)&m50chaiz!0i^4r3#@b*$_7w)q@q#e+c
 DEBUG = True
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['megahd.pythonanywhere.com']
 
 
 # Application definition
@@ -129,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
